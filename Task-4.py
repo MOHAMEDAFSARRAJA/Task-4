7e4a93821dc212ea4b1ab770cb01f6c51a816a7e
MOHAMEDAFSARRAJA <spiderboy59032@gmail.com>
2024-09-02 20:56:54 +0530

    Add files via upload

---
+++ Task-4.py
@@ -0,0 +1,69 @@
+def is_valid(board, row, col, num):
+    # Check if 'num' is not in the current row
+    for x in range(9):
+        if board[row][x] == num:
+            return False
+
+    # Check if 'num' is not in the current column
+    for x in range(9):
+        if board[x][col] == num:
+            return False
+
+    # Check if 'num' is not in the current 3x3 subgrid
+    start_row = row - row % 3
+    start_col = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    empty = find_empty_location(board)
+    if not empty:
+        return True  # Puzzle solved
+
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            # Undo the move
+            board[row][col] = 0
+
+    return False
+
+def find_empty_location(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def print_board(board):
+    for row in board:
+        print(" ".join(str(num) if num != 0 else '.' for num in row))
+
+
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(board):
+    print("Sudoku solved successfully:")
+    print_board(board)
+else:
+    print("No solution exists")
